54a1285a0d5f61a5b4ddd7cf0130fc4944068d5a
Alisa Tkaczyk <alisa.tk@gmail.com>
2017-11-05 12:55:02 -0500

    final copy for demo

--- js/kalbot.js
+++ js/kalbot.js
@@ -12,8 +12,8 @@ window.onload = function() {
         $('#chat-history').removeClass("chat-history");
         $('#chat-history').addClass("chat-history-mobile");
 
-    }
-  }).resize();
+      }
+    }).resize();
 
   $(init);
   function init(){
@@ -60,49 +60,60 @@ window.onload = function() {
       //bring up the next question
       //if both are empty then display the message - send whats on your mind?
       $.get(url+query, function(data, status){
-       console.log("THIS SHOULD BE EMPTY" + data.options);
+       console.log("Content returned from the server: " + data.options);
+       console.log("Message returned from the server: " + data.message);
        let newButtons = data.options;
        let message = data.message;
        let urlKeywords = "http"; //every string with http
       //after noticing the text leaves are empty, we verify the message isn't a link
-        if(message.includes(urlKeywords)){
-          var win = window.open(message, '_blank');
-          if (win) {
+      if(message.includes(urlKeywords) || message.includes("kidshelphone.ca")){
+        var win = window.open(message, '_blank');
+        if (win) {
               //Browser has allowed it to be opened
               win.focus();
-          } else {
+            } else {
               //Browser has blocked it
               alert('Please allow popups for this website');
-          }
+            }
           //message = "<a href='"+message+"' target='_blank'>"+message+"</a>";
           //console.log("********* THIS IS A FUCKING URL" + message);
-
         }
-       $("#question-field").text(message);
-       let tempCounter = data.options.length;
 
-       for(const index in newButtons){
-         //only make a button and display it when the text field has content
-         if(newButtons[index].text === "" || newButtons[index].text === undefined){
-          //show talk about something else button.
-          $("#button-"+index).toggleClass("hide");
-          tempCounter -=1;
-          console.log(tempCounter + " COUNTER");
+        $("#question-field").text(message);
+        let tempCounter = data.options.length;
+        console.log("Number of options to display: --- " + tempCounter);
+        if(tempCounter === 0||data.options === null || data.options ===[]){
+          console.log("inhereee eogmgggg");
+          for(let i = 0; i < 3; i++){
+            $("#button-"+i).toggleClass("hide");
+            tempCounter -=1;
+            console.log(tempCounter + " Decreased COUNTER");
 
-          if(tempCounter <= 0){
+            if(tempCounter <= 0){
             //there's no buttons so display the default button and add a link to it 
             $("#button-0").text("I want to start a new conversation.");
             $("#button-0").data( "link", "What's on your mind?");   
             $("#button-0").toggleClass("hide");
-       //     $("#button-1").text("Continue to Kids Help Phone Website!");
-        //    $("#button-1").data( "link", "https://www.kidshelphone.ca");   
-        //    $("#button-1").toggleClass("hide");
+            // $("#button-1").text("Continue to Kids Help Phone Website!");
+            // $("#button-1").data( "link", "https://www.kidshelphone.ca");   
+            // $("#button-1").toggleClass("hide");
           }
         }
-        // else if(newButtons[index].link ==="") {
-        //   console.log("******************** link is empty");
-        // } 
-        else {
+      }
+      console.log(data.options);
+      for(const index in newButtons){
+         //only make a button and display it when the text field has content
+         console.log("---- INSIDE THE LOOP");
+         console.log("newButtons[index].text" + newButtons[index].text);
+         //array is null array is empty or 
+         if(newButtons[index].text === "" || newButtons[index].text === undefined){
+          
+          console.log("---- INSIDE THE IF STATEMENT");
+          //show talk about something else button.
+          
+        } else if(newButtons[index].link ===""||newButtons[index].link ===undefined) {
+          console.log("******************** link is empty");
+        } else {
           $("#button-"+index).removeClass("hide");
           $("#button-"+index).text(newButtons[index].text);
           $("#button-"+index).data( "link", newButtons[index].link);        
@@ -110,11 +121,13 @@ window.onload = function() {
           updateScroll(); 
         }
       }
+
+      console.log("END --")
     });
     });
 //if link contains https then send to that link
 
-  });
+});
 
 }
 
