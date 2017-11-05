77fcce50fd33eae59b8bb141893751ba764d3725
Alisa Tkaczyk <alisa.tk@gmail.com>
2017-11-05 01:20:48 -0500

    updated scroll functionality some added styles

--- js/kalbot.js
+++ js/kalbot.js
@@ -1,5 +1,20 @@
 window.onload = function() {
   //on first click I sent "What's on your mind?"
+
+  $(window).resize(function() {
+        // This will fire each time the window is resized:
+        if($(window).width() >= 800) {
+            // if larger or equal
+            $('#chat-history').addClass("chat-history");
+            $('#chat-history').removeClass("chat-history-mobile");
+        } else {
+            // if smaller
+            $('#chat-history').removeClass("chat-history");
+            $('#chat-history').addClass("chat-history-mobile");
+
+        }
+    }).resize();
+
   $(init);
   function init(){
     var url = "http://kalbot-api.us-east-1.elasticbeanstalk.com/answer?response=";
@@ -17,59 +32,54 @@ window.onload = function() {
     });
     console.log("*** Init function completed ***");
   }
-
+  
+  function updateScroll(){
+    document.getElementById("chat-history").scrollTop = document.getElementById("chat-history").scrollHeight;
+    //document.getElementById("chat-history-mobile").scrollTop = document.getElementById("chat-history-mobile").scrollHeight;
+    $('html, body').scrollTop( $('body').height() );
+    console.log("------------------------------------ " + document.height());
+  }
   $(document).ready(function(){
     $("button").click(function(){
+      $("#chat-history").removeClass("hide"); // i only have to do this on the first click, but oh well.
       var url = "http://kalbot-api.us-east-1.elasticbeanstalk.com/answer?response=";
       var query =  $( "#"+this.id ).data( "link" );
-
-
       // console.log("SENDING THIS BACK TO REQUEST GET: "+query);
       // console.log(url+query);
+
       if(query === ""|| query === undefined){
-       //what to do if there is no link on the component.     
-      }
-
-      //LOG the recent response in the chat history as a smaller bubble.
-
-      var recentResponse = $( "#"+this.id ).text();
-      var $div = $("<div></div>");;
-      $("#chat-history").append($div);
+       //what to do if there is no link on the component.
+     }
       //log the previous question in the chat history as a smaller bubble.
+      var prevQuestion = $( "#question-field" ).text();
+      var $divQuestion = $("<h5 class='question cell'>" + prevQuestion + "</h5>");
+      $("#chat-history").append($divQuestion);
 
+      //log the recent response in the chat history as a smaller bubble.
+      var prevResponse = $( "#"+this.id ).text();
+      var $divResponse = $("<h5 class='answer archived cell'>"+prevResponse + "</h5>");
+      console.log($divResponse);
+      $("#chat-history").append($divResponse);
 
+      updateScroll(); 
       //bring up the next question
 
+      //only make a button and display it when the text field has content
+      //if the text is blank don't display the button
+      //if the link is blank dont look anything up
+      //if both are empty then display the message - send whats on your mind?
       $.get(url+query, function(data, status){
        // console.log(data);
-        let newButtons = data.options;
-        $("#question-field").text(data.message);
-        for(const index in newButtons){
-          $("#button-"+index).text(newButtons[index].text);
-          $("#button-"+index).data( "link", newButtons[index].link);
+       let newButtons = data.options;
+       $("#question-field").text(data.message);
+       for(const index in newButtons){
+        $("#button-"+index).text(newButtons[index].text);
+        $("#button-"+index).data( "link", newButtons[index].link);
           //console.log("link " + index + " --" + newButtons[index].link);
         }
-
-        //switch to text - enter phone number, and we send a text
-        //the buttons without links need functionality and what they can do is actually respond to what they are so
-
-
-          //console.log("this is the id: " + this.id);
-          //console.log(document.getElementById("question-field").innerHTML);
-          // $.get(url+query, function(data, status){
-          //   document.getElementById("question-field").innerHTML = data;
-          //   var newButtons = map.get(data);
-          //   for(const [i,button] of newButtons.entries()){
-          //     console.log(i);
-          //     console.log(button);
-          //     console.log("----");
-          //     console.log(newButtons.entries());
-          //     console.log(document.getElementById("button-1").innerHTML);
-          //     var temp = document.getElementById("button-"+i);
-          //     console.log("why is this poo?" + temp);
-          //   }
-            //alert("Data: " + data + "\nStatus: " + status);
-          });
+      });
     });
+
   });
+
 }
